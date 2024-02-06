5947528f5a5adb5714f8263609c3679906850823
SudeepKumarS <bhavasudeepkumar@gmail.com>
2024-02-06 21:35:04 +0530

    Added a simple GO backend application

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Helllo World!")
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func helloWorldPage(c *gin.Context) {
+	c.String(200, "Hell the World!!")
 }
 
 func main() {
-	http.HandleFunc("/", helloWorldPage)
-	http.ListenAndServe("8000", nil)
+	router := gin.Default()
+	router.GET("/", helloWorldPage)
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
 }
